e478e9b2e4d40affbbf8add9075627680c851c1f
dpavon-g <pavongomezdaniel@gmail.com>
2024-05-27 20:15:56 +0200

    Linear regression funcionando, solo falta ponerlo bonito y documentarlo

--- estimatePrice.py
+++ estimatePrice.py
@@ -6,15 +6,6 @@ def estimatePrice(mileage, theta0, theta1):
     estimatePrice = float(theta0) + (float(theta1) * mileage)
     return estimatePrice
 
-def drawGraphic(theta0, theta1):
-    x_values = np.linspace(0, 1, 100)
-    y_values = estimatePrice(x_values, theta0, theta1)
-    plt.plot(x_values, y_values, color='red', label='Línea de regresión')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.legend()
-    plt.title('Regresión Lineal')
-    plt.show()
 
 def main():
     with open("values.json", "r") as myJson:
@@ -28,8 +19,6 @@ def main():
     estimateCarPrice = estimatePrice(mileage, myData["Theta0"], myData["Theta1"])
 
     print(int(estimateCarPrice), "$")
-
-    drawGraphic(myData["Theta0"], myData["Theta1"])
 
 if __name__ == "__main__":
     main()
--- train.py
+++ train.py
@@ -1,6 +1,11 @@
+import sys
+import json
 import pandas as pd
 import matplotlib.pyplot as plt
 from estimatePrice import estimatePrice
+
+t0 = 0
+t1 = 0
 
 def normalize(series):
     min_val = series.min()
@@ -8,20 +13,55 @@ def normalize(series):
     normalized_data = (series - min_val) / (max_val - min_val)
     return normalized_data
 
-def trainTheta0(df):
+def denormalize(df, t0, t1):
+    
+    minPrice_val = df['price'].min()
+    maxPrice_val = df['price'].max()
+    minKm_val = df['km'].min()
+    maxKm_val = df['km'].max()
+    
+    t0_desnormalizado = 0;
+    t1_desnormalizado = 0;
+    
+    t0_desnormalizado = t0 * (maxPrice_val - minPrice_val) + minPrice_val +\
+    (t1 * minKm_val * (minPrice_val - maxPrice_val)) / (maxKm_val - minKm_val)
+    t1_desnormalizado = t1 * (maxPrice_val - minPrice_val) / (maxKm_val - minKm_val)
+    
+    return t0_desnormalizado, t1_desnormalizado
+
+def trainThetas(df, t0, t1):
     learningRate = 0.1
-    t0 = learningRate * (1/len(df))
-    t1 = learningRate * (1/len(df))
+    sumErrorT0 = 0
+    sumErrorT1 = 0
+    # global t0
+    # global t1
+    
+    
     for index, car in df.iterrows():
-        t0 = t0 + estimatePrice(car['km'], t0, t1) - car['price']
-        t1 = t1 + (estimatePrice(car['km'] - car['price'], t0, t1) * car['km'])
-    print("Theta0: ", t0)
-    print("Theta1: ", t1)
+        price = car['price']
+        km = car['km']
+        prediction = estimatePrice(km, t0, t1)
+        error = prediction - price
+        sumErrorT0 += error
+        sumErrorT1 += error * km
+        
+    t0 -= learningRate * 1 / len(df) * sumErrorT0
+    t1 -= learningRate * 1 / len(df) * sumErrorT1
+    
+    return (t0, t1)
+
+def writeThetas(jsonFile, t0, t1):
+    with open(jsonFile, 'w') as myJson:
+        json.dump({"Theta0": t0, "Theta1": t1}, myJson)
+
+# def calculateError(df, t0, t1):
+
 
 def main():
     csv_file = 'data.csv'
-    thetaJson = 'values.json'
     df = pd.read_csv(csv_file)
+    t0 = 0
+    t1 = 0
     
     normalized_data = {}
     normalized_data['price'] = normalize(df['price'])
@@ -33,7 +73,25 @@ def main():
     plt.ylabel('Price')
     plt.title('Price vs Kilometers')
 
-    trainTheta0(df_normalized)
+
+    while (True):
+        oldT0 = t0
+        oldT1 = t1
+        t0, t1 = trainThetas(df_normalized, t0, t1)
+        print("Theta0: ", t0)
+        print("Theta1: ", t1)
+        if (t0 == oldT0 and t1 == oldT1):
+            break
+        
+    price0 = estimatePrice(0, t0, t1)
+    price1 = estimatePrice(1, t0, t1)
+
+    plt.plot([0,1], [price0,price1], color='red')
+    
+    t0, t1 = denormalize(df, t0, t1)
+    
+    writeThetas('values.json', t0, t1)
+    
     plt.show()
 
 
